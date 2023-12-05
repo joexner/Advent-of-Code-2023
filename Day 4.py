d78bbdecfd268a3835b4c3707812beba14b7c647
Jonathan Oexner <jonathan.oexner@alum.wpi.edu>
2023-12-04 21:19:28 -0500

    Day 4 pt 2

--- Day 4.py
+++ Day 4.py
@@ -1,5 +1,6 @@
 import math
 import re
+from collections import deque
 
 filename = 'Day 4 - input.txt'
 
@@ -19,4 +20,26 @@ with open(filename) as file:
         total_points += points
 
 print(total_points)
+
+cards_read = 0
+
+values_queue = deque()
+
+with open(filename) as file:
+    for line in file:
+        num_copies_of_this_card = values_queue.popleft() if len(values_queue) > 0 else 1
+        line = line.strip()
+        numbers_and_bars_list = re.findall(r"\d+|\|", line)
+        bar_idx = numbers_and_bars_list.index("|")
+        winners = set(numbers_and_bars_list[1:bar_idx])
+        mine = set(numbers_and_bars_list[bar_idx + 1:])
+        num_wins = len(winners.intersection(mine))
+        for n in range(num_wins):
+            if n >= len(values_queue):
+                values_queue.append(1)
+            values_queue[n] += num_copies_of_this_card
+        cards_read += num_copies_of_this_card
+        # print(num_copies_of_this_card)
+
+print(cards_read)
 
